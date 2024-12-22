2f0af74e4ec3ad9161029ec76d9b2bec9e2fd3ac
shibadatta870 <dasshibadatta@gmail.com>
2024-12-22 19:53:34 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const config = {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-uplodes",
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://0vfq3ozzta.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_KXFnZaZHr",
+        APP_CLIENT_ID: "6cebsirb3n2nna8npa1la3hp6i",
+        IDENTITY_POOL_ID: "us-east-1:3418d962-e31c-44d7-a11d-af1f75c0fe6b",
+    },
+};
+export default config;
